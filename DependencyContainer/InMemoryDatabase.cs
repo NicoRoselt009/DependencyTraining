e8c1c1819a7261b5fdb6a9593c19a1b1190ebe84
NicoRoselt009 <56814237+NicoRoselt009@users.noreply.github.com>
2020-08-06 15:29:46 +0200

    Initial commit

---
+++ DependencyContainer/InMemoryDatabase.cs
@@ -0,0 +1,39 @@
+﻿using DependencyContainer.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DependencyContainer
+{
+    public class InMemoryDatabase
+    {
+        public void SeedData()
+        {
+            Persons = new List<Person>()
+            {
+                new Person()
+                {
+                    DateOfBirth = new System.DateTime(1992,10,15),
+                    Gender = "Male",
+                    LastName = "Roselt",
+                    Name = "Nico"
+                },
+                new Person()
+                {
+                    DateOfBirth = new System.DateTime(1994,10,15),
+                    Gender = "Male",
+                    LastName = "Slabbert",
+                    Name = "Brandon"
+                },
+                new Person()
+                {
+                    DateOfBirth = new System.DateTime(1950,10,15),
+                    Gender = "Male",
+                    LastName = "Lurie",
+                    Name = "Juan"
+                }
+            }.AsQueryable();
+        }
+
+        public IQueryable<Person> Persons { get; set; }
+    }
+}
---
+++ DependencyContainer/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DependencyContainer.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+
+    }
+}
